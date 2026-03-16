7498b334862f15b8ddd1eaa83d95735dcf94d4fe
Tanshi Sharma <120782697+TanshiSharma1999@users.noreply.github.com>
2026-03-16 20:38:52 +0530

    Implement selection sort for ascending and descending order
    Added functionality to sort the array in both ascending and descending order using selection sort.

--- BasicSelection.py
+++ BasicSelection.py
@@ -1,6 +1,7 @@
 # Selection Sort is a simple sorting algorithm. 
 # It repeatedly selects the smallest (or largest) element from the unsorted part of the array and places it in the correct position.
 
+#sorts min to max
 def selectionSort(array, size):
     for ind in range(size - 1):
         min_index = ind
@@ -15,4 +16,21 @@ arr = [-2, 45, 0, 11, -9, 88, -97, -202, 747]
 size = len(arr)
 selectionSort(arr, size)
 
-print(arr)
+print("Sorted Smallest to Largest: ",arr)
+
+#sorts max to min
+def selectionSort(array, size):
+    for ind in range(size - 1):
+        max_index = ind
+
+        for j in range(ind + 1, size):
+            if array[j] > array[max_index]:
+                max_index = j
+
+        array[ind], array[max_index] = array[max_index], array[ind]
+
+arr = [-2, 45, 0, 11, -9, 88, -97, -202, 747]
+size = len(arr)
+selectionSort(arr, size)
+
+print("Sorted  Largest to Smallest : ",arr)
